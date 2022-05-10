96e46bace392b6e6a5b0529f0bf7ef2908a91226
tiennguyen19 <51728075+tiennguyen19@users.noreply.github.com>
2022-05-10 13:34:40 +0700

    Update HinhTruTest.java

--- src/test/java/lesson16_HomeWork/HinhTruTest.java
+++ src/test/java/lesson16_HomeWork/HinhTruTest.java
@@ -10,8 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HinhTruTest {
 
-    HinhTron ht = new HinhTron();
-
     @DisplayName("Show exception if side is invalid")
     @ParameterizedTest(name = "Chieu cao {0}, ban kinh day {1} khong hop le")
     @CsvFileSource(resources = "/HinhTru_InvalidData.csv", numLinesToSkip = 1)
@@ -25,8 +23,6 @@ class HinhTruTest {
     @CsvFileSource(resources = "/HinhTru_ValidData.csv", numLinesToSkip = 1)
     void tinhThetich(double chieuCao, double banKinhDay, double dientich1, double dientich2, double expectedTheTich) {
         HinhTru hTru = new HinhTru(chieuCao, banKinhDay);
-        ht.setR(banKinhDay);
-        hTru.setH(chieuCao);
         double actual = (double)Math.round(hTru.tinhTheTich()*1000)/1000;
         Assertions.assertEquals(expectedTheTich, actual, 0.000001);
     }
@@ -35,8 +31,6 @@ class HinhTruTest {
     @CsvFileSource(resources = "/HinhTru_ValidData.csv", numLinesToSkip = 1)
     void tinhDienTichXungQuanh(double chieuCao, double banKinhDay, double expectedDienTichXungQuanh) {
         HinhTru hTru = new HinhTru(chieuCao, banKinhDay);
-        ht.setR(banKinhDay);
-        hTru.setH(chieuCao);
         double actual = (double)Math.round(hTru.tinhDienTichXungQuanh()*1000)/1000;
         Assertions.assertEquals(expectedDienTichXungQuanh, actual, 0.000001);
     }
@@ -45,8 +39,6 @@ class HinhTruTest {
     @CsvFileSource(resources = "/HinhTru_ValidData.csv", numLinesToSkip = 1)
     void tinhDienTich(double chieuCao, double banKinhDay, double __, double expectedDienTichToanPhan) {
         HinhTru hTru = new HinhTru(chieuCao, banKinhDay);
-        ht.setR(banKinhDay);
-        hTru.setH(chieuCao);
         double actual = (double) Math.round(hTru.tinhDienTichToanPhan()*1000)/1000;
         Assertions.assertEquals(expectedDienTichToanPhan, actual, 0.000001);
     }
